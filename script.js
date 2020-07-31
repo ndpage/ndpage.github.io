992011d62a4cd038754b89f77ff0bee68a64ec71
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-07-30 22:50:23 -0400

    New changed from personal-website repo

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
     $('.profile-img').click(function() { //Event listener for home button 
       alert("clicked image");
     });
+
+    $('.projects-btn').click(function() { //Event listener for home button 
+      //var contentMain = document.getElementById("#content-main")
+      
+      $.ajax(
+      { url: "http://127.0.0.1:3000/projects",
+        type: "GET",
+      success: function(result){
+          $("#content-main").html(result);
+        }
+      });
+    });
